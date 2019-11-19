c3eea8a7931cafa737e160808affadd6c312e0b4
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-19 15:32:03 +0900

    12_03

--- main.c
+++ main.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void){
-		int i = 0;
-		char str[4];
-		str[0] = 'a';
-		str[1] = 'b';
-		str[2] = 'c';
-		str[3] = '\0';
-	printf("%s\n", str);
+void main(void){
+	char src[] = "The Worst Things to Eat Before You Sleep";
+	char dst[100];
 	
-	while(str[i] != '\0'){
-		printf("%c", str[i]);
-		i++;
-	}	
-	return 0;
+	strcpy(dst,src);
+	
+	printf("copied string : %s", dst);
 }
